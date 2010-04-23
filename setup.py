7559e1067bf609fe4caaf0b75aad71c21d86bde5
Cristian Stoica <crististm@gmail.com>
2010-04-23 17:48:40 +0300

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+
+setup(name='pkssh',
+      version='0.1',
+      description='Python SSH utils',
+      author='Cristian Stoica',
+      author_email='crististm@gmail.com',
+      py_modules=['pkssh'],
+      license='GPLv2'
+      )
+
